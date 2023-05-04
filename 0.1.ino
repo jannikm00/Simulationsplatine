7cbd2b73c59888bdf02ead3d5616a39024464dfd
Jannik <72359748+jannikm00@users.noreply.github.com>
2023-05-04 17:17:03 +0200

    Update 0.5.ino

--- 0.1.ino
+++ 0.1.ino
@@ -13,7 +13,6 @@ int countlauflicht = 0; //Anzahl Durchläufe Lauflicht
 
 ///Programm Variablen///
 bool Anlage_ein = false;
-
 ///Simulierte Sensoren Werte, in void Grundstellung () verändern
 bool sB10 = false; //Simulierter Sensor
 bool sB11 = false; //"
@@ -21,7 +20,8 @@ bool sB12 = false; //"
 bool sB13 = false; //"
 bool sB14 = false; //"
 bool sB15 = false; //"
-bool M1 = false; //Motoren Abfrage variable NICHT VERÄNDERN!
+///Abgefragte Motoren Status Variablen
+bool M1 = false; //Motoren Abfrage Variable NICHT VERÄNDERN!
 bool M2 = false; //"
 bool M3 = false; //"
 bool M4 = false; //"
@@ -37,19 +37,19 @@ bool E1ok;
 bool Anlageok;
 
 ///Relais Output Pins
-int Q1 = 2;    //B10
-int Q2 = 3;    //B11
-int Q3 = 4;    //B12
-int Q4 = 5;    //B13
-int Q5 = 6;    //B14
-int Q6 = 7;    //B15
+int Q1 = 2;    // -B10 Output
+int Q2 = 3;    // -B11 Output
+int Q3 = 4;    // -B12 Output
+int Q4 = 5;    // -B13 Output
+int Q5 = 6;    // -B14 Output
+int Q6 = 7;    // -B15 Output
 
 //Motoren Input Pins
-int P10 = 12;  // -E1 input
-int P11 = 8;   // -M1 input
-int P12 = 9;   // -M2 input
-int P14 = 10;  // -M4 input
-int P15 = 11;  // -M3 input
+int P10 = 12;  // -E1 Input
+int P11 = 8;   // -M1 Input
+int P12 = 9;   // -M2 Input
+int P14 = 10;  // -M4 Input
+int P15 = 11;  // -M3 Input
 
 
 
@@ -112,9 +112,9 @@ void Programm() { //Funktion: Programm bis auf Füllstand hoch und niedrig in vo
     sB14 = true;  //B14 betätigen Signal Gebläse ok -->Heizung -E1 an
     Relais_check();
     if (E1 == true) {  //Heizung -E1 check
-      E1ok = true;
+      E1ok = true; //Debugging
     } else {
-      E1ok = false;
+      E1ok = false; //Debugging
     }
 
     if (E1ok == true) {  //Heizung RGB wird von Blau zu rot (fade)
@@ -139,7 +139,7 @@ void Programm() { //Funktion: Programm bis auf Füllstand hoch und niedrig in vo
     }
 
     if (M1 == true) {  //M1 check
-      M1ok = true;
+      M1ok = true; //Debugging
     if (sB12 == false) {
         delay(1000); //Delay Druckaufbau
       }
@@ -148,20 +148,20 @@ void Programm() { //Funktion: Programm bis auf Füllstand hoch und niedrig in vo
     } else {
       sB12 = false;
     Relais_check();
-      M1ok = false;
+      M1ok = false; //Debugging
     }
 
     if (M4 == true) {  //M4 check
-      M4ok = true;
+      M4ok = true; //Debugging
     } else {
-      M4ok = false;
+      M4ok = false; //Debugging
     }
 
     if (M3 == true) {  //Nach betätigen von -S3 --> Laufband -M3 geht an
-      M3_1ok = true;
+      M3ok = true; //Debugging
      //Niedriger Füllstand -B15 betätigen --> -M3 aus siehe void lauflichtm3 ()//
     } else {
-      M3_1ok = false;
+      M3ok = false; //Debugging
     }
 
   }
@@ -170,7 +170,7 @@ void Programm() { //Funktion: Programm bis auf Füllstand hoch und niedrig in vo
 
 
 
-void Relais_check() {  //Funktion: Übersetzung Schalter B10 --> Relais Q1
+void Relais_check() {  //Funktion: Übersetzung Schalter B10 --> Relais Q1, aktualisiert Relais
   if (sB10 == false) {
     analogWrite(Q1, 0);
   } else {
@@ -250,8 +250,8 @@ void printstatus() { //Funktion: Serieller Output für Debugging
   Serial.print(M1ok);
   Serial.write("|M2=");
   Serial.print(M2ok);
-  Serial.write("|!M3=");
-  Serial.print(M3_2ok);
+  Serial.write("|M3=");
+  Serial.print(M3ok);
   Serial.write("|M4=");
   Serial.print(M4ok);
   Serial.write("|E1=");
@@ -260,6 +260,30 @@ void printstatus() { //Funktion: Serieller Output für Debugging
     Serial.print("Prozessabfolge mit entleertem Füllturm abgeschlossen!           ");
   } else {
     Serial.print("Prozessabfolge abgeschlossen!                                   ");
+  }
+}
+
+
+
+
+
+void fuellstandm4() { //Funktion: Füllstand
+  if (M4ok == true) { //Ventil -M4 offen --> Füllstand erhöht sich
+    if (countfuellstand <= 8) {
+      led.setPixelColor(countfuellstand, led.Color(255, 255, 255));
+      led.show();
+      countfuellstand++;
+      delay(50);
+      if (countfuellstand >= 7) {
+        sB11 = true;  //Füllstand hoch
+        sB15 = false; //"
+        Relais_check();
+      } else if (countfuellstand <= 6) {
+        sB11 = false;  //Füllstand niedrig
+        sB15 = true;   //"
+        Relais_check();
+      }
+    }
   }
 }
 
@@ -294,26 +318,16 @@ void lauflichtm3() {  //Funktion: Förderschnecke Animation -M3
 
 
 
-void fuellstandm4() { //Funktion: Füllstand
-  if (M4ok == true) { //Ventil -M4 offen --> Füllstand erhöht sich
-    if (countfuellstand <= 8) {
-      led.setPixelColor(countfuellstand, led.Color(255, 255, 255));
-      led.show();
-      countfuellstand++;
-      delay(50);
-      if (countfuellstand >= 7) {
-        sB11 = true;  //Füllstand hoch
-        sB15 = false; //"
-        Relais_check();
-      } else if (countfuellstand <= 6) {
-        sB11 = false;  //Füllstand niedrig
-        sB15 = true;   //"
-        Relais_check();
-      }
-    }
+void pegelupdate() { //Funktion: Füllstand Pegel senken
+  for (int i = 5; i < 9; i++) {
+    led.setPixelColor(i, led.Color(0, 0, 0));
   }
+  led.show();
+  for (int i = 5; i < countfuellstand; i++) {
+    led.setPixelColor(i, led.Color(255, 0, 0));
+  }
+  led.show();
 }
-
 
 
 
@@ -331,26 +345,11 @@ void end() { //Funktion: Reset bei Anlage aus
     M3 = false;
     M4 = false;
     E1 = false;
-    Relais_check();
-    Serial.println("Anlage aus");
-    Programm();
-    count = 0;
+    Relais_check(); //Relais zurücksetzen
+    Serial.println("Anlage aus"); //Debugging
+    Programm(); //Neustart wenn Anlage_ein == true
+    count = 0; //void Grundstellung () darf einmal laufen
   }
-}
-
-
-
-
-
-void pegelupdate() { //Funktion: Füllstand Pegel senken
-  for (int i = 5; i < 9; i++) {
-    led.setPixelColor(i, led.Color(0, 0, 0));
-  }
-  led.show();
-  for (int i = 5; i < countfuellstand; i++) {
-    led.setPixelColor(i, led.Color(255, 0, 0));
-  }
-  led.show();
 }
 
 
@@ -358,14 +357,14 @@ void pegelupdate() { //Funktion: Füllstand Pegel senken
 
 
 void loop() {
-  Motor_check();
-  Anlage_ein_check();
+  Motor_check(); //Motoren Abfrage für Anlage_ein
+  Anlage_ein_check(); //Check ob -M2 an ist
   if (Anlage_ein == true) {
-    Motor_check();
-    Programm();
-    printstatus();
-    fuellstandm4();
-    lauflichtm3();
+    Motor_check(); //Motorenabfrage für Hauptprogramm
+    Programm(); //Hauptprogramm
+    printstatus(); //Debugging
+    fuellstandm4(); //Füllstand Animation
+    lauflichtm3(); //Förderschnecke Animation
   } else{
-  end();}
+  end();} //Reset
 }
